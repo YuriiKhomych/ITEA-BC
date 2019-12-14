79f290019a450ccfcfc4ed84dc69bb205ebbd290
keirinuniverse <57413409+keirinuniverse@users.noreply.github.com>
2019-12-14 14:28:48 +0200

    'homework5'

---
+++ Patenko_Victoria/5_functions/5_hw1.py
@@ -0,0 +1,11 @@
+def max_in_list(*nums):
+    max_num = max(nums)
+    print(max_num)
+
+max_in_list(1, 6, 57, 7, 78, 23, 345, 90, 56, 789)
+max_in_list(0, 7, 45, 5, 67, 8, 67)
+max_in_list(90, 67, 34, 345, 567, 678, 45)
+max_in_list(0)
+
+
+
